17e87682b671a48577bdd82ee9c4659cec2ea40f
Josh Lohner <lohnerj1@mymail.nku.edu>
2024-02-19 16:48:46 -0500

    Looks
    Improved the looks of login,register, and sign in pages

--- bookme/lib/Login_Pages/login_page.dart
+++ bookme/lib/Login_Pages/login_page.dart
@@ -1,6 +1,6 @@
-// login_page.dart
-import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api
 
+import 'package:flutter/material.dart';
 import '../contact_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,52 +11,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.teal,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                // Replace this check with your actual authentication logic
-                if (_emailController.text.isNotEmpty &&
-                    _passwordController.text.isNotEmpty) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ContactPage(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.blue,
+              Colors.teal
+            ], // Add your desired gradient colors
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.3, // Adjust the width as needed
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black), // Add border to TextField
+                    borderRadius: BorderRadius.circular(
+                        8.0), // Optional: Add border radius
+                  ),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border:
+                          InputBorder.none, // Hide TextField's default border
+                      contentPadding: EdgeInsets.all(16.0),
                     ),
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              ),
-              child: const Text('Login', style: TextStyle(fontSize: 20)),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.3, // Adjust the width as needed
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black), // Add border to TextField
+                    borderRadius: BorderRadius.circular(
+                        8.0), // Optional: Add border radius
+                  ),
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border:
+                          InputBorder.none, // Hide TextField's default border
+                      contentPadding: EdgeInsets.all(16.0),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_emailController.text.isNotEmpty &&
+                        _passwordController.text.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ContactPage(),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal, // Set background color
+                    foregroundColor: Colors.white, // Set text color to black
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                  ),
+                  child: const Text('Login', style: TextStyle(fontSize: 20)),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- bookme/lib/Login_Pages/register_page.dart
+++ bookme/lib/Login_Pages/register_page.dart
@@ -1,4 +1,3 @@
-// register_page.dart
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
@@ -20,52 +19,115 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text(
+          'Register',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.teal,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Name'),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                // Replace this check with your actual registration logic
-                if (_nameController.text.isNotEmpty &&
-                    _emailController.text.isNotEmpty &&
-                    _passwordController.text.isNotEmpty) {
-                  // Registration successful, navigate to login page
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginPage(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.lightBlue,
+              Colors.teal
+            ], // Add your desired gradient colors
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.8, // Adjust the width as needed
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black), // Add border to TextField
+                    borderRadius: BorderRadius.circular(
+                        8.0), // Optional: Add border radius
+                  ),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 16.0), // Adjust the padding as needed
                     ),
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              ),
-              child: const Text('Register', style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.8, // Adjust the width as needed
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black), // Add border to TextField
+                    borderRadius: BorderRadius.circular(
+                        8.0), // Optional: Add border radius
+                  ),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 16.0), // Adjust the padding as needed
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  width: MediaQuery.of(context).size.width *
+                      0.8, // Adjust the width as needed
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black), // Add border to TextField
+                    borderRadius: BorderRadius.circular(
+                        8.0), // Optional: Add border radius
+                  ),
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 16.0), // Adjust the padding as needed
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_nameController.text.isNotEmpty &&
+                        _emailController.text.isNotEmpty &&
+                        _passwordController.text.isNotEmpty) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal, // Set background color
+                    foregroundColor: Colors.white, // Set text color to black
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                  ),
+                  child: const Text('Register', style: TextStyle(fontSize: 20)),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
