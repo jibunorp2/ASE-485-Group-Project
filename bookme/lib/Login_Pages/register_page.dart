ab6b6cafef251ca14574f32a0175dec5a7d7a010
Josh Lohner <lohnerj1@mymail.nku.edu>
2024-04-15 15:58:48 -0400

    Messing with register to check the function

--- bookme/lib/Login_Pages/register_page.dart
+++ bookme/lib/Login_Pages/register_page.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:flutter/material.dart';
+import 'package:bookme/crud.dart';
+import 'package:flutter/material.dart'; // Import your CRUD class
 import 'login_page.dart'; // Import your login page
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({super.key});
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
   _RegisterPageState createState() => _RegisterPageState();
@@ -14,6 +13,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final CRUD _crud = CRUD(); // Create an instance of the CRUD class
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +28,7 @@ class _RegisterPageState extends State<RegisterPage> {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Colors.lightBlue,
-              Colors.teal
-            ], // Add your desired gradient colors
+            colors: [Colors.lightBlue, Colors.teal],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -43,84 +40,81 @@ class _RegisterPageState extends State<RegisterPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  width: MediaQuery.of(context).size.width *
-                      0.8, // Adjust the width as needed
+                  width: MediaQuery.of(context).size.width * 0.8,
                   decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.black), // Add border to TextField
-                    borderRadius: BorderRadius.circular(
-                        8.0), // Optional: Add border radius
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: TextField(
                     controller: _nameController,
                     decoration: const InputDecoration(
                       labelText: 'Name',
                       contentPadding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                          horizontal: 16.0), // Adjust the padding as needed
+                          vertical: 16.0, horizontal: 16.0),
                     ),
                   ),
                 ),
                 const SizedBox(height: 16),
                 Container(
-                  width: MediaQuery.of(context).size.width *
-                      0.8, // Adjust the width as needed
+                  width: MediaQuery.of(context).size.width * 0.8,
                   decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.black), // Add border to TextField
-                    borderRadius: BorderRadius.circular(
-                        8.0), // Optional: Add border radius
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: TextField(
                     controller: _emailController,
                     decoration: const InputDecoration(
                       labelText: 'Email',
                       contentPadding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                          horizontal: 16.0), // Adjust the padding as needed
+                          vertical: 16.0, horizontal: 16.0),
                     ),
                     keyboardType: TextInputType.emailAddress,
                   ),
                 ),
                 const SizedBox(height: 16),
                 Container(
-                  width: MediaQuery.of(context).size.width *
-                      0.8, // Adjust the width as needed
+                  width: MediaQuery.of(context).size.width * 0.8,
                   decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.black), // Add border to TextField
-                    borderRadius: BorderRadius.circular(
-                        8.0), // Optional: Add border radius
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: TextField(
                     controller: _passwordController,
                     decoration: const InputDecoration(
                       labelText: 'Password',
                       contentPadding: EdgeInsets.symmetric(
-                          vertical: 16.0,
-                          horizontal: 16.0), // Adjust the padding as needed
+                          vertical: 16.0, horizontal: 16.0),
                     ),
                     obscureText: true,
                   ),
                 ),
                 const SizedBox(height: 32),
-                //Register Button
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (validateName(_nameController.text) &&
                         validateEmail(_emailController.text) &&
                         validatePass(_passwordController.text)) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ),
+                      bool success = await _crud.signUp(
+                        _nameController.text,
+                        _emailController.text,
+                        _passwordController.text,
                       );
+                      if (success) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      } else {
+                        // Handle sign up failure
+                        // You can show a snackbar or error message here
+                      }
                     }
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal, // Set background color
-                    foregroundColor: Colors.white, // Set text color to black
+                    backgroundColor: Colors.teal,
+                    foregroundColor: Colors.white,
                     padding: const EdgeInsets.symmetric(
                         horizontal: 40, vertical: 20),
                   ),
@@ -135,19 +129,18 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 }
 
-//returns true if at least 1 number, 1 letter, and 7 characters
+// Returns true if at least 1 number, 1 letter, and 7 characters
 bool validatePass(String password) {
   return RegExp(r'^(?=.*[a-zA-Z])(?=.*\d).{7,}$').hasMatch(password);
 }
 
-//returns true if email follows x@y.z format
+// Returns true if email follows x@y.z format
 bool validateEmail(String email) {
   return RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
       .hasMatch(email);
 }
 
-//returns true if only lettters are found and is not empty
+// Returns true if only letters are found and is not empty
 bool validateName(String name) {
-  // Check if the name is not blank and contains only letters
   return name.isNotEmpty && !RegExp(r'[^a-zA-Z]').hasMatch(name);
 }
