3769e3ad4409def106f03e318206b61c22928940
ScramblesDev <beesley02@gmail.com>
2024-04-21 12:52:48 -0400

    primitive appointment scheduling

--- bookme/lib/Message_Pages/message_page.dart
+++ bookme/lib/Message_Pages/message_page.dart
@@ -31,9 +31,71 @@ class _MessagePageState extends State<MessagePage> {
   }
 
   void _loadMessages() async {
-    var fetchedMessages = await _crud.getMessageList(widget.chatRoomId);
-    setState(() {
-      messages = fetchedMessages;
+    try {
+      QuerySnapshot snapshot = await _crud.getMessageList(widget.chatRoomId);
+      List<Map<String, dynamic>> fetchedMessages =
+          snapshot.docs.map((DocumentSnapshot doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        data['id'] =
+            doc.id; // Include document ID if needed for operations like update
+        return data;
+      }).toList();
+
+      setState(() {
+        messages = fetchedMessages;
+      });
+    } catch (e) {
+      print("Error loading messages: $e");
+    }
+  }
+
+  void _sendMessage() async {
+    if (messageController.text.isNotEmpty) {
+      await _crud.sendMessage(widget.chatRoomId, messageController.text);
+      messageController.clear();
+      _loadMessages(); // Reload the message list to include the new message
+    }
+  }
+
+  void addAppointmentDetails(String details) async {
+    // Get the current user ID as the sender
+    String currentUserUID = _crud.currentUserUid;
+
+    try {
+      // Fetch participant UIDs for the current chat room
+      List<String> participantUIDs =
+          await _crud.getParticipantUIDs(widget.chatRoomId);
+
+      // Check if current user is part of participants; add if not
+      if (!participantUIDs.contains(currentUserUID)) {
+        participantUIDs.add(currentUserUID);
+      }
+
+      // Concatenate appointment details with participant info for clarity or use as needed
+      String messageDetails =
+          "$details, Participants: ${participantUIDs.join(', ')}";
+
+      // Send appointment message including the participants
+      _crud
+          .sendAppointmentMessage(
+              widget.chatRoomId, messageDetails, participantUIDs)
+          .then((_) {
+        _loadMessages(); // Reload messages to show the new appointment
+      }).catchError((error) {
+        print("Failed to add appointment details: $error");
+      });
+    } catch (e) {
+      print("Error processing appointment details: $e");
+    }
+  }
+
+  void _updateAppointmentStatus(String messageId, String status) {
+    _crud
+        .updateAppointmentStatus(widget.chatRoomId, messageId, status)
+        .then((_) {
+      _loadMessages(); // Refresh messages to reflect the updated status
+    }).catchError((error) {
+      print("Failed to update appointment status: $error");
     });
   }
 
@@ -49,10 +111,43 @@ class _MessagePageState extends State<MessagePage> {
             child: ListView.builder(
               itemCount: messages.length,
               itemBuilder: (context, index) {
+                var message =
+                    messages[index]; // Define message here for correct access
                 return ListTile(
-                  title: Text(messages[index]['text']),
+                  title: Text(message['text']),
                   subtitle: Text(
-                      'Sent at: ${messages[index]['timestamp'].toDate().toString()}'),
+                      'Sent at: ${message['timestamp'].toDate().toString()}'),
+                  trailing: message['type'] == 'appointment' &&
+                          message['status'] == 'pending'
+                      ? Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.check),
+                              onPressed: () {
+                                // Call the approveAppointment function
+                                _crud
+                                    .approveAppointment(
+                                  widget.chatRoomId,
+                                  messages[index][
+                                      'id'], // Ensure message ID is correctly retrieved
+                                )
+                                    .then((_) {
+                                  _loadMessages(); // Refresh messages to reflect the updated status
+                                }).catchError((error) {
+                                  print(
+                                      "Failed to approve appointment: $error");
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.close),
+                              onPressed: () => _updateAppointmentStatus(
+                                  message['id'], 'rejected'),
+                            ),
+                          ],
+                        )
+                      : null,
                 );
               },
             ),
@@ -131,20 +226,5 @@ class _MessagePageState extends State<MessagePage> {
         ],
       ),
     );
-  }
-
-  void _sendMessage() async {
-    if (messageController.text.isNotEmpty) {
-      await _crud.sendMessage(widget.chatRoomId, messageController.text);
-      messageController.clear();
-      _loadMessages(); // Reload the message list to include the new message
-    }
-  }
-
-  void addAppointmentDetails(String details) {
-    setState(() {
-      messages.add({"text": details, "timestamp": Timestamp.now()});
-      showAppointmentButtons = true; // Show the buttons after appointment added
-    });
   }
 }
--- bookme/lib/crud.dart
+++ bookme/lib/crud.dart
@@ -1,4 +1,5 @@
 // crud.dart
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:math'; // For generating random numbers
@@ -10,15 +11,13 @@ class CRUD {
   // Expose currentUserUid for external use
   String get currentUserUid => _auth.currentUser?.uid ?? "";
 
-  // Function to register a new user
+  // Inside CRUD class
   Future<bool> registerUser(String name, String email, String password) async {
     try {
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
       User? user = userCredential.user;
       await user!.updateDisplayName(name);
-
-      // Generate a unique 6-digit user ID
       String userID = _generateUserID();
 
       // Save user info in Firestore
@@ -32,6 +31,12 @@ class CRUD {
         'contact_list_ID': user.uid,
         'user_ID': userID,
         'users_in_list': [],
+      });
+
+      // Create a new appointment list for the user
+      await _firestore.collection('appointment_lists').doc(user.uid).set({
+        'appointment_list_ID': user.uid,
+        'appointments': [],
       });
 
       return true;
@@ -121,11 +126,9 @@ class CRUD {
   // Helper function to generate a random 6-digit user ID
   String _generateUserID() {
     var random = Random();
-    String id = '';
-    for (int i = 0; i < 6; i++) {
-      id += random.nextInt(10).toString();
-    }
-    return id;
+    int max = 1000000; // Making sure the ID has exactly 6 digits
+    int idNum = random.nextInt(max);
+    return idNum.toString().padLeft(6, '0'); // Ensure the ID is 6 digits long
   }
 
   // Ensure chat rooms are identified using only Firebase Auth UIDs
@@ -164,23 +167,17 @@ class CRUD {
     });
   }
 
-  // Function to fetch messages
-  Future<List<Map<String, dynamic>>> getMessageList(String chatRoomID) async {
-    var messages = await _firestore
+  Future<QuerySnapshot> getMessageList(String chatRoomID) {
+    // This should query Firestore and return a QuerySnapshot
+    return _firestore
         .collection('chat_rooms')
         .doc(chatRoomID)
         .collection('messages')
-        .orderBy('timestamp', descending: true)
+        .orderBy('timestamp')
         .get();
-
-    return messages.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
   }
 
-  // CRUD.dart
-
-// Method to get user UID by user_ID
+  // Method to get user UID by user_ID
   Future<String> getUserUIDByUserID(String userID) async {
     var userQuery = await _firestore
         .collection('users')
@@ -192,6 +189,106 @@ class CRUD {
       return userQuery.docs.first.id; // Returning the UID of the user
     } else {
       throw Exception('User not found');
+    }
+  }
+
+  // In crud.dart
+  Future<void> sendAppointmentMessage(String chatRoomID, String messageDetails,
+      List<String> participantUIDs) async {
+    DocumentReference messageRef = _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .doc();
+
+    await messageRef.set({
+      'id': messageRef.id,
+      'sender_id':
+          _auth.currentUser?.uid, // ID of the user who sends the appointment
+      'text': messageDetails,
+      'type': 'appointment', // Identifying this message as an appointment
+      'timestamp': FieldValue.serverTimestamp(),
+      'status': 'pending', // Initial status of the appointment
+      'participants': participantUIDs, // Include participants in the message
+    });
+  }
+
+  Future<void> updateAppointmentStatus(
+      String chatRoomID, String messageID, String newStatus) async {
+    await _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .doc(messageID)
+        .update({'status': newStatus});
+  }
+
+  Future<void> approveAppointment(String chatRoomId, String messageId) async {
+    DocumentReference messageRef = _firestore
+        .collection('chat_rooms')
+        .doc(chatRoomId)
+        .collection('messages')
+        .doc(messageId);
+    DocumentSnapshot messageDoc = await messageRef.get();
+
+    if (messageDoc.exists && messageDoc.data() != null) {
+      Map<String, dynamic> messageData =
+          messageDoc.data()! as Map<String, dynamic>;
+      if (messageData['status'] == 'pending' &&
+          messageData.containsKey('participants')) {
+        List<String> participantUIDs =
+            List<String>.from(messageData['participants']);
+        if (participantUIDs.isNotEmpty) {
+          var appointment = {
+            'appointment_ID':
+                Random().nextInt(1000000).toString().padLeft(6, '0'),
+            'title': messageData['title'],
+            'type': messageData['type'],
+            'date': messageData['date'],
+            'time': messageData['time'],
+            'details': messageData['details']
+          };
+
+          // Add appointment to both users' lists
+          for (String uid in participantUIDs) {
+            await _firestore.collection('appointment_lists').doc(uid).update({
+              'appointments': FieldValue.arrayUnion([appointment])
+            });
+          }
+
+          // Update the message status to 'approved'
+          await messageRef.update({'status': 'approved'});
+        } else {
+          throw Exception(
+              "Participants data missing or invalid in the appointment message");
+        }
+      } else {
+        throw Exception("Appointment is already processed or data is missing.");
+      }
+    } else {
+      throw Exception("Appointment message not found or data is null");
+    }
+  }
+
+  // Method in CRUD to get participant UIDs from a chat room
+  Future<List<String>> getParticipantUIDs(String chatRoomId) async {
+    try {
+      DocumentSnapshot chatRoomDoc =
+          await _firestore.collection('chat_rooms').doc(chatRoomId).get();
+      if (chatRoomDoc.exists && chatRoomDoc.data() != null) {
+        Map<String, dynamic> data = chatRoomDoc.data() as Map<String, dynamic>;
+        if (data.containsKey('participants') && data['participants'] is List) {
+          // Convert all items in the list to String, assuming they are stored as such
+          return List<String>.from(data['participants']);
+        } else {
+          throw Exception("Participants field is missing or not a list");
+        }
+      } else {
+        throw Exception("Chat room does not exist or has no data");
+      }
+    } catch (e) {
+      print("Failed to fetch participant UIDs: $e");
+      throw Exception("Failed to fetch participant UIDs");
     }
   }
 }
